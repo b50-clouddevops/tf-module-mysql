03176c3491b95d2ea47180bf8dc28459c7605ae9
mvpc-11232 <mvp2612@hotmail.com>
2022-10-29 06:16:52 +0530

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -2,14 +2,12 @@ resource "null_resource" "cluster" {
 
   provisioner "local-exec" {
     command = <<EOF 
-
 cd /tmp/
 curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
 unzip mysql.zip 
 cd mysql-main 
-mysql -h ${aws_db_instance.mysql.address} -uadmin1 -pRoboShop1
-
-EOF 
+mysql -h ${aws_db_instance.mysql.address} -uadmin1 -pRoboShop1 < shipping.sql
+EOF
 
   }
 }
